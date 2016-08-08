2a87299c1ab7793da5edb39e6ec1847bf79132f5
lab2 <lab2@tcslab.com>
2016-08-08 13:53:06 +0530

    Added new code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 /* eslint-disable no-console */
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './search_bar';
+
 console.log('hi');
+// const App = function(){
+//   return <h2>It Works...Hurray</h2>;
+// };
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+export default App;
---
+++ src/search_bar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SeacrhBar extends Component {
+  render(){
+    return (
+      <div>
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+export default SeacrhBar;
